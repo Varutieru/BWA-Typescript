ac1e7041b81b81f902ea14c2ca2b77a6f0f2a7b7
Varutieru <151426935+Varutieru@users.noreply.github.com>
2025-10-12 02:26:36 +0700

    Task 3: Any

--- build/typeDasar.js
+++ build/typeDasar.js
@@ -3,12 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // string
 let nama = "tahsasta";
 nama = "string";
-//number
+// number
 let umur = 20;
 umur = 30;
 umur = 13;
-//boolean
+// boolean
 let isMarried;
 isMarried = false;
 isMarried = true;
+// any
+let heroes = "Iron Man";
+heroes = 20;
+heroes = [];
+heroes = {};
+heroes = true || false || "string";
 //# sourceMappingURL=typeDasar.js.map
--- src/typeDasar.ts
+++ src/typeDasar.ts
@@ -2,12 +2,19 @@
 let nama: string = "tahsasta";
 nama = "string";
 
-//number
+// number
 let umur: number = 20;
 umur = 30
 umur = 13;
 
-//boolean
+// boolean
 let isMarried: boolean;
 isMarried = false;
 isMarried = true;
+
+// any
+let heroes: any = "Iron Man";
+heroes = 20;
+heroes = [];
+heroes = {}
+heroes = true || false || "string";
