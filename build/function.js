22fc91470900e45feacab2c309eb9e459275bb15
Varutieru <151426935+Varutieru@users.noreply.github.com>
2025-10-12 02:47:06 +0700

    Task 7: Functions

---
+++ build/function.js
@@ -0,0 +1,15 @@
+"use strict";
+// tipe data pada balikan function
+Object.defineProperty(exports, "__esModule", { value: true });
+function getName() {
+    return "hello, my name is tahsasta";
+}
+console.log(getName());
+function getAge() {
+    return 20;
+}
+function printName() {
+    console.log("print name");
+}
+printName();
+//# sourceMappingURL=function.js.map
---
+++ src/function.ts
@@ -0,0 +1,17 @@
+// tipe data pada balikan function
+
+function getName(): string {
+    return "hello, my name is tahsasta"
+}
+
+console.log(getName());
+
+function getAge(): number {
+    return 20;
+}
+
+function printName(): void {
+    console.log("print name");
+}
+
+printName();
