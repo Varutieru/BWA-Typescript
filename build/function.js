2a4f30b74577b3426133e7e2d7071f49f967427c
Varutieru <151426935+Varutieru@users.noreply.github.com>
2025-10-12 02:51:53 +0700

    task 8: Arguments

--- build/function.js
+++ build/function.js
@@ -12,4 +12,10 @@ function printName() {
     console.log("print name");
 }
 printName();
+/////////////////
+function multiply(val1, val2) {
+    return val1 * val2;
+}
+const result = multiply(20, 5);
+console.log(result);
 //# sourceMappingURL=function.js.map
