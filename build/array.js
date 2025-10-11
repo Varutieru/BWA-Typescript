cc37d6255b9ddc0ceb501f4c10066a4baa547f76
Varutieru <151426935+Varutieru@users.noreply.github.com>
2025-10-12 02:30:07 +0700

    Task 4: Array

---
+++ build/array.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// array
+let array;
+array = [1, 2, 3];
+let array2;
+array2 = ['string1', 'string2', 'string3'];
+let array3;
+array3 = [1, 'string', true, {}];
+//# sourceMappingURL=array.js.map
