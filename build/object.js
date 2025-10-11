df28f3a13026d6aa3045b0c1a73a05b3162a4fd5
Varutieru <151426935+Varutieru@users.noreply.github.com>
2025-10-12 03:01:49 +0700

    Task 10: Object

---
+++ build/object.js
@@ -0,0 +1,8 @@
+"use strict";
+// Object
+Object.defineProperty(exports, "__esModule", { value: true });
+let user = {
+    name: "Tahsasta",
+    age: 20
+};
+//# sourceMappingURL=object.js.map
---
+++ src/object.ts
@@ -0,0 +1,11 @@
+// Object
+
+type User = {
+    name: String,
+    age: number;
+}
+
+let user: User = {
+    name: "Tahsasta",
+    age: 20
+};
