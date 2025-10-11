28897dddc6a2ee345060cbe71d91a494b19fbb80
Varutieru <151426935+Varutieru@users.noreply.github.com>
2025-10-12 02:24:25 +0700

    Task 2: Number and Boolean

--- build/typeDasar.js
+++ build/typeDasar.js
@@ -1,5 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// string
 let nama = "tahsasta";
 nama = "string";
+//number
+let umur = 20;
+umur = 30;
+umur = 13;
+//boolean
+let isMarried;
+isMarried = false;
+isMarried = true;
 //# sourceMappingURL=typeDasar.js.map
--- src/typeDasar.ts
+++ src/typeDasar.ts
@@ -1,3 +1,13 @@
+// string
 let nama: string = "tahsasta";
-
 nama = "string";
+
+//number
+let umur: number = 20;
+umur = 30
+umur = 13;
+
+//boolean
+let isMarried: boolean;
+isMarried = false;
+isMarried = true;
